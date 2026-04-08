aa4a4673e0ed34f063c9d34ce1615e47698ae60e
AshishSharma975 <ashish8389sharma@gmail.com>
2026-04-08 20:50:43 +0530

    feat: implement authentication controller with registration, login, refresh, and session-based logout functionality

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -124,7 +124,7 @@ export async function getMe(req, res) {
         const token = req.headers.authorization?.split(" ")[1];
 
         if (!token) {
-            return res.status(401).json({
+            return res.status(400).json({
                 message: "token not found"
             });
         }
